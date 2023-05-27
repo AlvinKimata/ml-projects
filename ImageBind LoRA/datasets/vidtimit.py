1b5481c8a6b924caa277448b911aeb1d3b65e4e8
Alvin Kimata <alvinkimata@gmail.com>
2023-05-27 21:40:25 +0300

    add text modality type

--- ImageBind LoRA/datasets/vidtimit.py
+++ ImageBind LoRA/datasets/vidtimit.py
@@ -1,4 +1,5 @@
 import os
+from imutils import paths as path
 from typing import Optional, Callable
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
@@ -15,24 +16,33 @@ class VIDTIMITDataset(Dataset):
         self.classes = [d for d in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, d))]
         self.class_to_idx = {cls: idx for idx, cls in enumerate(self.classes)}
 
+        '''
         self.paths = []
-        self.audio_paths = []
+        for cls in self.classes:
+            cls_dir = os.path.join(root_dir, cls)
+            for filename in os.listdir(cls_dir):
+                if filename.endswith('.jpg'):
+                    self.paths.append((os.path.join(cls_dir, filename), cls))
+        '''
+        self.paths = []
         self.video_paths = []
 
         for cls in self.classes:
             cls_dir = os.path.join(root_dir, cls)
-            cls_audio_dir = os.path.join(cls_dir, 'audio')
-            cls_video_dir = os.path.join(cls_dir, 'videos')
+            cls_video_dir = path.list_files(cls_dir)
 
-            for filename in os.listdir(cls_audio_dir):
-              if filename.endswith('.wav'):
-                self.audio_paths.append(os.path.join(cls_audio_dir, filename)), 
-
-            for filename in os.listdir(cls_video_dir):
+            
+            for filename in cls_video_dir:
+              filename = str(filename)
               if filename.endswith('.avi'):
-                self.video_paths.append(os.path.join(cls_video_dir, filename))
-        self.paths = list(zip(self.audio_paths, self.video_paths))
+                video_directory = filename
 
+                audio_id = filename.split(sep = '.')[0]
+                audio_path = audio_id + '.wav'
+                audio_file = os.path.split(audio_path)[-1]
+                audio_directory = os.path.join(root_dir, 'audio', audio_file)
+
+                self.paths.append((video_directory, audio_directory, cls))
           
         #Split dataset.
         train_paths, test_paths = train_test_split(self.paths, train_size=train_size, random_state=random_seed)
@@ -48,10 +58,10 @@ class VIDTIMITDataset(Dataset):
         return len(self.paths)
     
     def __getitem__(self, index):
-        audio_path, video_path = self.paths[index]
+        video_path, audio_path, cls = self.paths[index]
         
         audios = data.load_and_transform_audio_data([audio_path], self.device)
         videos = data.load_and_transform_video_data([video_path], self.device)
-        # texts = data.load_and_transform_text([class_text], self.device)
+        text = data.load_and_transform_text([cls], self.device)
 
-        return audios, ModalityType.AUDIO, videos, ModalityType.VISION
+        return audios, ModalityType.AUDIO, videos, ModalityType.VISION, text, ModalityType.TEXT
