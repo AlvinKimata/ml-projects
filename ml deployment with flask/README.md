3a763be7b141cc7ba30760765e8efb01a0d5b126
AlvinKimata <alvinkimata@gmail.com>
2023-09-10 01:39:22 +0300

    test application

--- ml deployment with flask/README.md
+++ ml deployment with flask/README.md
@@ -1,2 +1,16 @@
 ## This project involves building and deploying a machine learning application with flask as an API endpoint.
 ## Reference to this project can be found [here.](https://www.datacamp.com/tutorial/machine-learning-models-api-python)
+
+### The command below uses cURL command to perform a POST request to the endpoint to perform inference.
+```bash
+curl -X POST -H "Content-Type: application/json" -d '{"Age": 30, "Sex": "male", "Embarked": "C"}' http://localhost:12345/predict
+```
+
+### The output or result of the command below is returned as a json object: 
+
+```text
+{
+  "prediction": "[0]"
+}
+
+```
--- ml deployment with flask/main.py
+++ ml deployment with flask/main.py
@@ -13,13 +13,13 @@ def predict():
         try:
             json_ = request.json
             print(json_)
-            query = pd.get_dummies(pd.DataFrame(json_))
+            query = pd.get_dummies(pd.DataFrame(json_, index = [0]))
             query = query.reindex(columns=model_columns, fill_value = 0)
 
             prediction = list(lr.predict(query))
             return jsonify({'prediction': str(prediction)})
         except:
-            return jsonify({'trace': traceback.format_exec()})
+            return jsonify({'trace': traceback.format_exc()})
     else:
         print("No model to use here.")
 
@@ -30,7 +30,11 @@ if __name__ == '__main__':
     except:
         port = 12345
 
-    lr = joblib.load('models/model_columns.pkl')
+    lr = joblib.load("models/model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("models/model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
+
     
     
     app.run(debug = True, port = port)
