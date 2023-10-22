0a8144c9477930a8cfb023983c3deaf5c2f8d6f5
AlvinKimata <alvinkimata@gmail.com>
2023-10-22 18:59:16 +0300

    function for applying  rotary embeddings

--- llama/model.py
+++ llama/model.py
@@ -22,6 +22,22 @@ class ModelArgs:
 
     device: str = None
 
+def precompute_theta_pos_frequencies(head_dim:int, seq_len: int, device:str, theta: float = 10000.0):
+	assert head_dim % 2 == 0, "Dimension must be divisible by 2."
+	theta_numerator = torch.arange(0, head_dim, 2).float()
+	theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device)
+	m = torch.arange(seq_len, device = device)
+	freqs = torch.outer(m, theta).float()
+	freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
+
+def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device:str):
+	x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+	freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2)
+	x_rotated = x_complex * freqs_complex
+	x_out = torch.view_as_real(x_rotated)
+	x_out = x_out.reshape(*x.shape)
+	return x_out.type_as(x).to(device)
+
 class Transformer(nn.Module):
 	def __init(self, args = ModelArgs):
 		super().__init__()
@@ -59,6 +75,6 @@ class Transformer(nn.Module):
 		#Apply normalization.
 		h = self.norm(h)
 		
-		#Apply grads to softmax to get output.
+		#Apply grads to a linear layer and softmax to get output.
 		output = self.output(h).float()
 		return output
