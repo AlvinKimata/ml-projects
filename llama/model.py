4469f0531a0e7e6fad7e032f9f9d4a225f83b3e5
AlvinKimata <alvinkimata@gmail.com>
2023-10-22 19:10:15 +0300

    add RMSNorm class

--- llama/model.py
+++ llama/model.py
@@ -38,6 +38,21 @@ def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device
 	x_out = x_out.reshape(*x.shape)
 	return x_out.type_as(x).to(device)
 
+
+class RMSNorm(nn.Module):
+	def __init__(self, dim: int, eps: float = 1e-4):
+		super().__init__()
+		self.eps = eps
+		self.weight = nn.Parameter(torch.ones(dim))
+
+	def _norm(self, x: torch.Tensor):
+		return x * torch.rsqrt(x.pow(2).mean(-1, keepdim = True) + self.eps)
+	
+	def forward(self, x: torch.Tensor):
+		return self.weight * self._norm(x.float()).type_as(x)
+	
+
+		
 class Transformer(nn.Module):
 	def __init(self, args = ModelArgs):
 		super().__init__()
